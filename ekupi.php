2cc706f41e106fb45d7b7458196f90d5d1a7c7ba
Alen Šimec <alen@eburza.hr>
2018-03-19 11:04:20 +0100

    Add files via upload

---
+++ ekupi.php
@@ -0,0 +1,59 @@
+﻿<?php
+    print '
+	<?xml version="1.0" encoding="utf-8"?>
+    <items>
+		<item>
+			<Sifra></Sifra>
+			<Naziv></Naziv>
+			<Oznaka1></Oznaka1>
+			<Oznaka2></Oznaka2>
+			<Oznaka3></Oznaka3>
+			<Opis><![CDATA[]]></Opis>
+			<Specifikacija><![CDATA[Brand: Brand §Tip proizvoda §Tip §Težina §Tip proizvoda]]></Specifikacija>
+			<Kolicina></Kolicina>
+			<Vrijedi-do></Vrijedi-do>
+			<Nabavna-cijena></Nabavna-cijena>
+			<Preporucena-cijena></Preporucena-cijena>
+			<Kategorija1></Kategorija1>
+			<Kategorija2></Kategorija2>
+			<Kategorija3></Kategorija3>
+			<Kategorija4></Kategorija4>
+			<Kategorija5></Kategorija5>
+			<Slika1></Slika1>
+			<Slika2></Slika2>
+			<Slika3></Slika3>
+			<Slika4></Slika4>
+			<Slika5></Slika5>
+			<Slika6></Slika6>
+			<Slika7></Slika7>
+			<Slika8></Slika8>
+			<Slika9></Slika9>
+			<Slika10></Slika10>
+			<Porez>25</Porez>
+			<Povratna-naknada></Povratna-naknada>
+			<Dobavljivost>2</Dobavljivost>
+        </item>
+		<item>
+			<Sifra></Sifra>
+			<Naziv></Naziv>
+			<Oznaka1></Oznaka1>
+			<Oznaka2></Oznaka2>
+			<Oznaka3></Oznaka3>
+			<Opis><![CDATA[]]></Opis>
+			<Specifikacija><![CDATA[Brand: §Tip proizvoda: ]]></Specifikacija>
+			<Kolicina></Kolicina>
+			<Vrijedi-do></Vrijedi-do>
+			<Nabavna-cijena></Nabavna-cijena>
+			<Preporucena-cijena></Preporucena-cijena>
+			<Kategorija1></Kategorija1>
+			<Kategorija2></Kategorija2>
+			<Kategorija3></Kategorija3>
+			<Kategorija4></Kategorija4>
+			<Kategorija5></Kategorija5>
+			<Slika></Slika>
+			<Porez>25</Porez>
+			<Povratna-naknada></Povratna-naknada>
+			<Dobavljivost>2</Dobavljivost>
+		</item>
+	</items>';
+?>
